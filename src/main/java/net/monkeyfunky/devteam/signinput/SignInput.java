1a4e446980ead861b847af4e2792b09ca1093729
eight_y_88 <jadec.eight@gmail.com>
2021-11-02 19:24:54 +0900

    [Init] プロジェクトの作成

---
+++ src/main/java/net/monkeyfunky/devteam/signinput/SignInput.java
@@ -0,0 +1,15 @@
+package net.monkeyfunky.devteam.signinput;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SignInput extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
